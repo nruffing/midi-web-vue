dc7ed077b2fd5c40af3fd65092d1ad5fef3f1810
Nicholas Ruffing <nicholasruffing70@gmail.com>
2023-10-08 08:09:52 -0400

    handle permission check

--- src/stores/devicesStore.ts
+++ src/stores/devicesStore.ts
@@ -1,17 +1,29 @@
 import { defineStore } from 'pinia'
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_MIDI_API
+
 export interface State {
   outputs: MIDIOutput[] | undefined,
+  midiPermissionRequired: boolean,
 }
 
 export const useDevicesStore = defineStore('devices', {
   state: (): State => ({
     outputs: undefined,
+    midiPermissionRequired: false,
   }),
   actions: {
     async initialize() {
-      const midiAccess = await navigator.requestMIDIAccess()
-      this.outputs = Array.from(midiAccess.outputs.values()) ?? []
+      // https://developer.mozilla.org/en-US/docs/Web/API/Permissions_API#browser_compatibility
+      const permission = await navigator.permissions.query({ name: "midi" , sysex: false } as unknown as PermissionDescriptor)
+      const isPrompt = permission.state === "prompt"
+      if (permission.state === "granted" || isPrompt) {
+        // requesting access when state is "prompt" will prompt user for permission
+        const midiAccess = await navigator.requestMIDIAccess()
+        this.outputs = Array.from(midiAccess.outputs.values()) ?? []
+      } else if (isPrompt) {
+        this.midiPermissionRequired = true
+      }
     },
   },  
 })
