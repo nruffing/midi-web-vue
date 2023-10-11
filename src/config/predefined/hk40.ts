212be5a226b908bda8cf0fbd8c70bdf31089b25e
Nicholas Ruffing <nicholasruffing70@gmail.com>
2023-10-11 08:16:46 -0400

    more grandmeister 40 and tonex config

--- src/config/predefined/hk40.ts
+++ src/config/predefined/hk40.ts
@@ -9,7 +9,47 @@ export const Grandmeister40Config = {
   specUrl: 'https://hughes-and-kettner.com/wp-content/uploads/2016/09/GM_40_Deluxe_BDA_1_3.pdf',
 
   controlChangeCommands: [
+    ControlChangeCommand.toggleCommand('Mute', 9),
+    
+    // Channel
+    ControlChangeCommand.rangeCommand('Channel', 31), // TODO - multi select commands
+    ControlChangeCommand.rangeCommand('Volume (soft)', 7),
+    ControlChangeCommand.rangeCommand('Volume (hard)', 57),
+    ControlChangeCommand.rangeCommand('Gain (soft)', 20),
+    ControlChangeCommand.rangeCommand('Gain (hard)', 56),
     ControlChangeCommand.toggleCommand('Boost', 64),
-    // TODO
+
+    // Power Amp EQ
+    ControlChangeCommand.rangeCommand('Resonance', 24),
+    ControlChangeCommand.rangeCommand('Presence', 25),
+
+    // Power Soak
+    // ControlChangeCommand.rangeCommand('Power Soak', 30), // TODO - multi select commands
+
+    // Preamp EQ
+    ControlChangeCommand.rangeCommand('Bass', 21),
+    ControlChangeCommand.rangeCommand('Mid', 22),
+    ControlChangeCommand.rangeCommand('Treble', 23),
+
+    // Modulation FX
+    ControlChangeCommand.toggleCommand('Mod-FX', 52),
+    ControlChangeCommand.rangeCommand('Mod-FX Type', 12), // TODO - multi select commands
+    ControlChangeCommand.rangeCommand('Modulation Intensity', 1),
+    ControlChangeCommand.rangeCommand('Mod Speed', 26),
+
+    // Delay FX
+    ControlChangeCommand.toggleCommand('Delay', 53),
+    ControlChangeCommand.rangeCommand('Delay Feedback', 27),
+    ControlChangeCommand.rangeCommand('Delay Volume', 28),
+
+    // Reverb FX
+    ControlChangeCommand.toggleCommand('Reverb', 54),
+    ControlChangeCommand.rangeCommand('Reverb Volume', 29),
+
+    // FX Loop
+    ControlChangeCommand.toggleCommand('FX Loop', 55),
+
+    // Noise Gate
+    ControlChangeCommand.toggleCommand('Noise Gate', 63),
   ],
 } as Controllable
--- src/config/predefined/tonex.ts
+++ src/config/predefined/tonex.ts
@@ -17,15 +17,45 @@ export const TonexConfig = {
   controlChangeCommands: [
     ControlChangeCommand.toggleCommand('Preset', 12),
     ControlChangeCommand.rangeCommand('Expression Pedal', 11),
-    ControlChangeCommand.rangeCommand('Gain', 102),
-    ControlChangeCommand.rangeCommand('Bass', 23),
-    ControlChangeCommand.rangeCommand('Mid', 24),
-    ControlChangeCommand.rangeCommand('Treble', 25),
-    ControlChangeCommand.toggleCommand('Reverb', 75),
-    ControlChangeCommand.toggleCommand('Compressor', 18),
-    ControlChangeCommand.toggleCommand('Noise Gate', 14),
+    
+    //ControlChangeCommand.rangeCommand('Volumne', ??),
     ControlChangeCommand.rangeCommand('Model Volume', 103),
-    // TODO
+    ControlChangeCommand.rangeCommand('Gain', 102),
+    
+    ControlChangeCommand.rangeCommand('Bass', 23),
+    ControlChangeCommand.rangeCommand('Bass Freq', 24),
+    ControlChangeCommand.rangeCommand('Mid', 24), // Same as Bass Freq??
+    ControlChangeCommand.rangeCommand('Mid Freq', 27),
+    ControlChangeCommand.rangeCommand('Mid Q', 26),
+    ControlChangeCommand.rangeCommand('Treble', 25),
+    ControlChangeCommand.rangeCommand('Treble Freq', 29),
+
+    ControlChangeCommand.rangeCommand('Presence', 106),
+    ControlChangeCommand.rangeCommand('Depth', 107),
+
+    ControlChangeCommand.toggleCommand('EQ Patch', 30), // toggle or range?
+    ControlChangeCommand.rangeCommand('Mix', 104),
+    
+    // Reverb
+    ControlChangeCommand.toggleCommand('Reverb', 75), 
+    ControlChangeCommand.rangeCommand('Reverb Type', 85), // TODO - multi select commands
+    ControlChangeCommand.rangeCommand('Reverb Time', 76),
+    ControlChangeCommand.rangeCommand('Reverb Pre Delay', 77),
+    ControlChangeCommand.rangeCommand('Reverb Color', 78),
+    ControlChangeCommand.rangeCommand('Reverb Type', 85),
+
+    // Noise Gate
+    ControlChangeCommand.toggleCommand('Noise Gate', 14),
+    ControlChangeCommand.rangeCommand('NG Release', 16),
+    ControlChangeCommand.rangeCommand('NG Depth', 17),
+    
+    // Compressor
+    ControlChangeCommand.toggleCommand('Compressor', 18),
+    ControlChangeCommand.rangeCommand('Comp Gain', 20),
+    ControlChangeCommand.rangeCommand('Comp Attack', 21),
+    ControlChangeCommand.rangeCommand('Comp Threshold', 22), // CMP PATC??
+
+    // TODO: Mic Commands    
   ],
 
   /*
